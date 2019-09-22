41e64824efc3840e863b77c36e76e7bd3549e2ac
Zetos Lima <cesar_manoel@hotmail.com>
2019-09-22 00:09:19 -0300

    EditRecipe form.

--- src/views/EditRecipe.vue
+++ src/views/EditRecipe.vue
@@ -1,6 +1,40 @@
 <template>
   <div v-if="recipe" class="container edit-recipe">
     <h2>Edit {{ recipe.title }} Recipe</h2>
+    <form @submit.prevent="editRecipe">
+      <div class="field title">
+        <label for="title">Recipe Title:</label>
+        <input type="text" name="title" v-model="recipe.title" />
+      </div>
+      <div
+        v-for="(ingredient, index) in recipe.ingredients"
+        :key="index"
+        class="field"
+      >
+        <label for="ingredient">Ingredient:</label>
+        <input
+          type="text"
+          name="ingredient"
+          v-model="recipe.ingredients[index]"
+        />
+        <i @click="deleteIngredient(ingredient)" class="material-icons delete"
+          >delete</i
+        >
+      </div>
+      <div class="field edit-ingredient">
+        <label for="edit-ingredient">Recipe Ingredients:</label>
+        <input
+          type="text"
+          name="edit-ingredient"
+          @keydown.tab.prevent="addIngredient"
+          v-model="another"
+        />
+      </div>
+      <div class="field center-align">
+        <p v-if="feedback" class="red-text">{{ feedback }}</p>
+        <button class="btn pink">Update Recipe</button>
+      </div>
+    </form>
   </div>
 </template>
 
@@ -11,8 +45,29 @@ export default {
   name: 'EditRecipe',
   data() {
     return {
-      recipe: null
+      recipe: null,
+      another: null,
+      feedback: null
     };
+  },
+  methods: {
+    editRecipe() {
+      console.log('recipe:', this.recipe);
+    },
+    addIngredient() {
+      if (this.another) {
+        this.recipe.ingredients.push(this.another);
+        this.another = null;
+        this.feedback = null;
+      } else {
+        this.feedback = 'You must enter a value to add an ingredient.';
+      }
+    },
+    deleteIngredient(item) {
+      this.recipe.ingredients = this.recipe.ingredients.filter(
+        ingredient => item !== ingredient
+      );
+    }
   },
   created() {
     // this is just to test the ".where" it should be .doc(id) on a real implementation.
@@ -29,4 +84,29 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.edit-recipe {
+  margin-top: 60px;
+  padding: 20px;
+  max-width: 500px;
+}
+
+.edit-recipe h2 {
+  font-size: 2em;
+  margin: 20px auto;
+}
+
+.edit-recipe .field {
+  margin: 20px auto;
+  position: relative;
+}
+
+.edit-recipe .delete {
+  position: absolute;
+  right: 0;
+  bottom: 16px;
+  color: #aaa;
+  font-size: 1.4em;
+  cursor: pointer;
+}
+</style>
